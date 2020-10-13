269b8968ab38ac864c69f26f336edd3d8a7f3781
David Suchý <Suchy.David@stud.vassboskovice.cz>
2020-10-13 10:39:05 +0200

    Rozhraní

---
+++ Pokladna/Pokladna/IRepos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokladna
+{
+   public interface IRepos
+    {
+         List<PokladniZaznam> Nactivse();
+        PokladniZaznam NactiZaznam(int IdPokladniZaznam);
+        PokladniZaznam VytvorZaznam(PokladniZaznam pokladniZaznam);
+        void UpravZaznam(PokladniZaznam pokladniZaznam);
+        void SmazZaznam(PokladniZaznam pokladniZaznam);
+    }
+}
---
+++ Pokladna/Pokladna/JsonRepos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokladna
+{
+    class JsonRepos : IRepos
+    {
+        public List<PokladniZaznam> Nactivse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PokladniZaznam NactiZaznam(int IdPokladniZaznam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SmazZaznam(PokladniZaznam pokladniZaznam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpravZaznam(PokladniZaznam pokladniZaznam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PokladniZaznam VytvorZaznam(PokladniZaznam pokladniZaznam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
